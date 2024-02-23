f4c1f914781b2d2f3ab3199d91bc9e21fbe271d9
Thales Davi de Souza <thalesdavisoul@gmail.com>
2024-02-23 06:10:09 -0300

    Quizz-Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizzComponent } from './components/quizz/quizz.component';
+import { BtnComponent } from './components/common/btn/btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuizzComponent,
+    BtnComponent
   ],
   imports: [
     BrowserModule,
